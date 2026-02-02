25fb2cacc2f6f0123d6f845d581b99a0a7865ff5
shreyamane1526 <shreyamane1526@gmail.com>
2026-02-02 06:23:52 +0530

    Delete Downloads/SignSpeak directory

--- Downloads/SignSpeak/inheritance/backend/src/routes/courseRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {getCourse} from '../controllers/getCourse.js'
-const router=express.Router();
-
-router.get("/:courseId",getCourse)
-
-export default router;
--- Downloads/SignSpeak/inheritance/backend/src/routes/lectureRoute.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import {getLecture} from '../controllers/getLecture.js'
-const router=express.Router();
-
-router.get("/:lecctureId",getLecture)
-
-export default router; 
